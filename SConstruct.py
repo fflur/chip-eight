4afc299389e7fc39a6c2d8601a940a758ff8697e
Please <m.shakeel9038@gmail.com>
2025-09-11 00:38:26 +0530

    Uses config.py to address header and library path issue. All header and library path should be specified in a config.py file which is written by anyone who is setting this project locally. No changes should be made to SConstruct.py in context with path 'cause it can make portability issue.

--- SConstruct.py
+++ SConstruct.py
@@ -1,16 +1,17 @@
+import config
+
 # Setting the environment construction variable.
 # CXX implies C++.
 evrm = Environment(
     # Using GNU C++ compiler.
-    CXX = 'g++',
-
-    # Using C++ 11 standard. Doesn't work for some reason.
-    # CXXVERSION = '11',
-
-    # Suppress warnings.
-    # Using C++20 standard.
-    CXXFLAGS = '-std=c++11 -w'
+    CXX = "g++",
+    # header files path
+    CPPPATH = config.header_path,
+    # library files path
+    LIBPATH = config.library_path,
+    # Libraries names without 'lib' prefix and '.a' suffix
+    LIBS = ["SDL2main", "SDL2"]
 )
 
 Export('evrm')
-SConscript('src/SConscript.py', variant_dir = 'build')
+SConscript("src/SConscript.py", variant_dir = "build")
--- src/SConscript.py
+++ src/SConscript.py
@@ -1,31 +1,7 @@
 Import('evrm')
-import os
 
 # Setting the decider function to use 'content' as value.
 evrm.Decider('content')
-
-# MSYS2 installation directory
-msys2_inst_drct = os.path.join(
-    'C:\\Users', # Necessary for Windows paths
-    'pc',
-    'applications',
-    'MSYS2',
-    'mingw64'
-)
-
-# Include paths for header files
-incl_path = [
-    'include',
-    os.path.join(msys2_inst_drct, 'include')
-]
-
-# Library path for linking
-lib_path = os.path.join(msys2_inst_drct, 'lib')
-
-# Appending include and library paths, and libraries to link against.
-evrm.Append(CPPPATH = evrm.Dir(incl_path))
-evrm.Append(LIBPATH = evrm.Dir(lib_path))
-evrm.Append(LIBS = ["mingw32", "SDL2main", "SDL2"])  # Link against SDL2
 
 def CompileToObject():
     # Creating object files. `CPPPATH` is used for implicit dependencies.
