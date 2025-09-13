9b3cb7fbb91d32368eda016af55a0db167800221
Please <m.shakeel9038@gmail.com>
2025-09-13 15:10:55 +0530

    function name changed in SConscript

--- src/SConscript.py
+++ src/SConscript.py
@@ -4,16 +4,18 @@ import os
 # Setting the decider function to use 'content' as value.
 evrm.Decider('content')
 
-def CompileToObject():
+def Compile():
     # registers.hpp object file
     rgtr_ob = evrm.Object('cpp/registers.cpp')
     # memory.hpp object file
     mmry_ob = evrm.Object('cpp/memory.cpp')
     # chip_exceptions.hpp object file
     xcps_ob = evrm.Object('cpp/chip_exceptions.cpp')
+    stck_ob = evrm.Object('cpp/stack.cpp')
 
     # Memory depends on the chip exceptions
     evrm.Depends(mmry_ob, xcps_ob)
+    evrm.Depends(stck_ob, xcps_ob)
     return
 
-CompileToObject()
+Compile()
