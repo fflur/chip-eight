5201da0c2b616ad83b6b06c5d879af8697fa1fc5
Please <m.shakeel9038@gmail.com>
2025-09-15 23:57:46 +0530

    I don't remember

--- src/cpp/memory.cpp
+++ src/cpp/memory.cpp
@@ -1,7 +1,7 @@
 #include <memory.hpp>
 
 Memory::Memory() {
-    u8 fonts[] = {
+    std::array<u8, 80> fonts = {
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
         0x20, 0x60, 0x20, 0x20, 0x70, // 1
         0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -20,48 +20,19 @@ Memory::Memory() {
         0xF0, 0x80, 0xF0, 0x80, 0x80, // F
     };
 
-    u8 font_size = sizeof(fonts)/sizeof(u8);
-    for (u8 i = 0; i < font_size; i++) this->mmry[i] = fonts[i];
+    for (u8 i = 0; i < fonts.size(); i++) this->mmry[i] = fonts[i];
 }
 
-void Memory::checkMemoryAccess(u16 addr) const {
-    if (addr <= 0x01FF || addr >= 0x0FFF) throw MemoryException(addr);
+bool Memory::isMemoryAccessLegal(u16 addr) const {
+    if (addr <= 0x01FF || addr >= 0x0FFF) return false;
+    return true;
 }
 
-// TODO: Change to std::array
-void Memory::read(
-    u16 addr,
-    std::array<u8, MAX_ROM_SIZE>& bytes_array,
-    u8 arr_len
-) const {
-    for (u8 i = 0; i < arr_len; i++)
-        bytes_array[i] = this->mmry[addr + i];
-}
-
-void Memory::write(u16 addr, u8* bytes, u8 arr_len) {
-    for (u8 i = 0; i < arr_len; i++) {
-        this->checkMemoryAccess(addr + i);
-        this->mmry[addr + i] = *(bytes + i);
-    }
-}
-
-void  Memory::writeByte(u16 mory_addr, u8 value) {
-    this->checkMemoryAccess(mory_addr);
-    this->mmry[mory_addr] = value;
+u8 Memory::read(u16 mory_addr) const {
+    if (!this->isMemoryAccessLegal(mory_addr)) throw MemoryException(mory_addr);
+    return this->mmry[mory_addr];
 }
 
 u16 Memory::getFontAddr(u8 hex_font_val) const {
     return 0x000 + (hex_font_val * 5);
-}
-
-void Memory::loadGame(std::array<u8, MAX_ROM_SIZE> rom_file) {
-    std::copy(
-        rom_file.begin(), // Source start
-        rom_file.end(), // Source end
-        this->mmry.begin() + ROM_STARTING_POS // Destination start (index 512)
-    );
-}
-
-u16 Memory::fetchInstruction(u16 prog_cntr) const {
-    return (this->mmry[prog_cntr] << 8) | this->mmry[prog_cntr + 1];
 }
--- src/include/memory.hpp
+++ src/include/memory.hpp
@@ -12,14 +12,16 @@ class Memory {
     private:
     std::array<u8, MEMORY_SIZE> mmry;
 
-    void checkMemoryAccess(u16) const;
+    bool isMemoryAccessLegal(u16) const;
 
     public:
     Memory();
-    void read(u16, u8*, u8) const; // TODO: Change to std::array
-    void write(u16, u8*, u8);
-    void writeByte(u16 addr, u8 num);
+    u8 read(u16 mory_addr) const;
+    void write(u16 addr, u8 num);
     u16 getFontAddr(u8) const;
-    void loadGame(std::array<u8, MAX_ROM_SIZE>);
-    u16 fetchInstruction(u16) const;
 };
+
+// The read function will have two versions. One where it reads a single
+// byte from the memory and another version where it reads multiple bytes
+// from the memory and returns them as an array.
+// The same applies to the write function.
